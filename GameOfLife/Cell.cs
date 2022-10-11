5672864500c9aefef6e7836e39154a13586caa89
tim0-12432 <79634593+tim0-12432@users.noreply.github.com>
2022-10-11 16:47:59 +0200

    working

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,143 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace GameOfLife;
+
+public class Cell
+{
+    private readonly Ellipse _ellipse;
+    private bool _gameStarted;
+    
+    public int X { get; set; }
+    public int Y { get; set; }
+    
+    public State CellState { get; set; }
+    
+    public enum State
+    {
+        Unselected,
+        Alive,
+        Dying,
+        NotAlive
+    }
+    
+    public delegate void CellStateChangeHandler(Cell cell, State state);
+    public event CellStateChangeHandler CellStateChange;
+    
+    public Ellipse Ellipse
+    {
+        get { return _ellipse; }
+    }
+    
+    public Cell(Canvas canvas, int gameWidth, int gameHeight, int x, int y)
+    {
+        X = x;
+        Y = y;
+        CellState = State.Unselected;
+        
+        _gameStarted = false;
+        _ellipse = new Ellipse();
+        _ellipse.Width = canvas.ActualWidth / gameWidth - 1;
+        _ellipse.Height = canvas.ActualHeight / gameHeight - 1;
+        _ellipse.Fill = Brushes.Cyan;
+        _ellipse.DataContext = this;
+        _ellipse.MouseEnter += ColorHover;
+        _ellipse.MouseLeave += ColorNormal;
+        _ellipse.MouseLeftButtonDown += SelectCell;
+        Canvas.SetLeft(_ellipse, x * canvas.ActualWidth / gameWidth);
+        Canvas.SetTop(_ellipse, y * canvas.ActualHeight / gameHeight);
+        canvas.Children.Add(_ellipse);
+    }
+    
+    private void SelectCell(object sender, MouseButtonEventArgs e)
+    {
+        Ellipse cell = (Ellipse)sender;
+        Cell cellData = (Cell)cell.DataContext;
+        if (!_gameStarted)
+        {
+            if (cellData.CellState == State.Unselected)
+            {
+                CellSelect(cellData, cell);
+            }
+            else
+            {
+                CellUnselect(cellData, cell);
+            }
+        }
+    }
+
+    public void CellSelect(Cell cell, Ellipse body)
+    {
+        cell.CellState = State.Alive;
+        body.Fill = Brushes.LimeGreen;
+        CellStateChange(cell, cell.CellState);
+    }
+    
+    public void CellUnselect(Cell cell, Ellipse body)
+    {
+        cell.CellState = State.Unselected;
+        body.Fill = Brushes.Cyan;
+        CellStateChange(cell, cell.CellState);
+    }
+
+    public void DrawStartSimulation()
+    {
+        _gameStarted = true;
+        _ellipse.MouseLeftButtonDown -= SelectCell;
+        _ellipse.MouseEnter -= ColorHover;
+        _ellipse.MouseLeave -= ColorNormal;
+        if (CellState == State.Unselected)
+        {
+            _ellipse.Fill = Brushes.Transparent;
+            CellState = State.NotAlive;
+        }
+    }
+    
+    public void DrawDie()
+    {
+        CellState = State.Dying;
+        _ellipse.Fill = Brushes.Red;
+        CellStateChange(this, CellState);
+    }
+    
+    public void DrawNotAlive()
+    {
+        CellState = State.NotAlive;
+        _ellipse.Fill = Brushes.Transparent;
+        CellStateChange(this, CellState);
+    }
+    
+    public void DrawBorn()
+    {
+        CellState = State.Alive;
+        _ellipse.Fill = Brushes.LimeGreen;
+        CellStateChange(this, CellState);
+    }
+    
+    private void ColorHover(object sender, MouseEventArgs e)
+    {
+        Ellipse ellipse = (Ellipse)sender;
+        Cell cell = (Cell)ellipse.DataContext;
+        if (cell.CellState == State.Unselected)
+        {
+            ellipse.Fill = Brushes.DodgerBlue;
+        } else if (cell.CellState == State.Alive)
+        {
+            ellipse.Fill = Brushes.ForestGreen;
+        }
+    }
+
+    private void ColorNormal(object sender, MouseEventArgs e) {
+        Ellipse ellipse = (Ellipse)sender;
+        Cell cell = (Cell)ellipse.DataContext;
+        if (cell.CellState == State.Unselected)
+        {
+            ellipse.Fill = Brushes.Cyan;
+        } else if (cell.CellState == State.Alive)
+        {
+            ellipse.Fill = Brushes.LimeGreen;
+        }
+    }
+}
---
+++ GameOfLife/MainWindow.xaml.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace GameOfLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _width;
+        private int _height;
+        private int _speed;
+        
+        private DispatcherTimer _timer;
+        
+        private int _days = 0;
+        private int _alive = 0;
+        private int _dead = 0;
+        private bool _isRunning = false;
+        private Dictionary<int, KeyValuePair<int, int>> statistics = new Dictionary<int, KeyValuePair<int, int>>();
+        
+        private Cell[] _cells;
+        private Cell[,] _cells2D;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartBtn.Visibility = Visibility.Hidden;
+            RandomBtn.Visibility = Visibility.Hidden;
+            StopBtn.Visibility = Visibility.Hidden;
+            ResumeBtn.Visibility = Visibility.Hidden;
+            LifeCount.Visibility = Visibility.Hidden;
+            DeadCount.Visibility = Visibility.Hidden;
+            DayCount.Visibility = Visibility.Hidden;
+            
+            _width = 30;
+            _height = 30;
+            _speed = 1500;
+            
+            SizeSlider.Value = _width;
+            SizeLbl.Content = "Size: " + _width;
+            
+            SpeedSlider.Value = ((double)_speed / 100);
+            SpeedLbl.Content = "Speed: " + SpeedSlider.Value;
+            
+            _timer = new DispatcherTimer();
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void CreateBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            CreateBtn.Visibility = Visibility.Hidden;
+            Options.Visibility = Visibility.Hidden;
+            _cells = new Cell[_width * _height];
+            _cells2D = new Cell[_width, _height];
+            _timer.Interval = TimeSpan.FromMilliseconds(_speed);
+            CreateCanvas(_width, _height);
+            RandomBtn.Visibility = Visibility.Visible;
+        }
+        
+        
+        private void RandomBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < _cells.Length; i++)
+            {
+                int number = random.Next(0, 3);
+                if (number == 1)
+                {
+                    _cells[i].CellSelect(_cells[i], _cells[i].Ellipse);
+                }
+                else
+                {
+                    _cells[i].CellUnselect(_cells[i], _cells[i].Ellipse);
+                }
+            }
+        }
+
+        private void StartBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            _isRunning = true;
+            StartBtn.Visibility = Visibility.Hidden;
+            RandomBtn.Visibility = Visibility.Hidden;
+            StopBtn.Visibility = Visibility.Visible;
+            DeadCount.Visibility = Visibility.Visible;
+            DayCount.Visibility = Visibility.Visible;
+            
+            for (int i = 0; i < _cells.Length; i++)
+            {
+                _cells[i].DrawStartSimulation();
+            }
+            
+            _timer.Start();
+        }
+        
+        private void StopBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            StopBtn.Visibility = Visibility.Hidden;
+            ResumeBtn.Visibility = Visibility.Visible;
+        }
+        
+        private void ResumeBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+            ResumeBtn.Visibility = Visibility.Hidden;
+            StopBtn.Visibility = Visibility.Visible;
+        }
+        
+        private void CreateCanvas(int width, int height)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Cell cell = new Cell(Canvas, width, height, x, y);
+                    cell.CellStateChange += Cell_CellStateChange;
+                    _cells2D[x, y] = cell;
+                    _cells[x * height + y] = cell;
+                }
+            }
+            LifeCount.Visibility = Visibility.Visible;
+        }
+
+        private void Cell_CellStateChange(Cell cell, Cell.State state)
+        {
+            int dying = 0;
+            int living = 0;
+            for (int i = 0; i < _cells.Length; i++)
+            {
+                Cell current = _cells[i];
+                if (current.CellState == Cell.State.Dying)
+                {
+                    dying++;
+                }
+                else if (current.CellState == Cell.State.Alive)
+                {
+                    living++;
+                }
+            }
+            
+            _alive = living;
+            _dead = dying;
+            
+            LifeCount.Content = "Alive: " + _alive;
+            DeadCount.Content = "Dying: " + _dead;
+
+            if (!_isRunning && _alive > 0)
+            {
+                CreateBtn.Visibility = Visibility.Hidden;
+                StartBtn.Visibility = Visibility.Visible;
+            } else if (!_isRunning && _alive == 0)
+            {
+                CreateBtn.Visibility = Visibility.Hidden;
+                StartBtn.Visibility = Visibility.Hidden;
+            }
+        }
+        
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            _days++;
+            int[,] neighbours = new int[_width, _height];
+            for (int x = 0; x < _width; x++)
+            {
+                for (int y = 0; y < _height; y++)
+                {
+                    neighbours[x, y] = GetNeighbours(x, y);
+                    
+                    if (_cells2D[x, y].CellState == Cell.State.Alive)
+                    {
+                        if (neighbours[x, y] < 2 || neighbours[x, y] > 3)
+                        {
+                            _cells2D[x, y].DrawDie();
+                        }
+                    }
+                    else if (_cells2D[x, y].CellState == Cell.State.NotAlive)
+                    {
+                        if (neighbours[x, y] == 3)
+                        {
+                            _cells2D[x, y].DrawBorn();
+                        }
+                    }
+                    else if (_cells2D[x, y].CellState == Cell.State.Dying)
+                    {
+                        _cells2D[x, y].DrawNotAlive();
+                    }
+                }
+            }
+            DayCount.Content = "Day: " + _days;
+            statistics.Add(_days, new KeyValuePair<int, int>(_alive, _dead));
+        }
+
+        private int GetNeighbours(int x, int y)
+        {
+            int neighbourCount = 0;
+                    
+            int xAbove = x - 1;
+            int xBelow = x + 1;
+            int yAbove = y - 1;
+            int yBelow = y + 1;
+
+            if (xAbove < 0)
+            {
+                xAbove = _width - 1;
+            }
+            if (xBelow >= _width)
+            {
+                xBelow = 0;
+            }
+            if (yAbove < 0)
+            {
+                yAbove = _height - 1;
+            }
+            if (yBelow >= _height)
+            {
+                yBelow = 0;
+            }
+            
+            if (_cells2D[xAbove, yAbove].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[x, yAbove].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[xAbove, y].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[xBelow, yBelow].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[x, yBelow].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[xBelow, y].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[xAbove, yBelow].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+            if (_cells2D[xBelow, yAbove].CellState == Cell.State.Alive)
+            {
+                neighbourCount++;
+            }
+
+            return neighbourCount;
+        }
+
+        private void SizeSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _width = (int) SizeSlider.Value;
+            _height = (int) SizeSlider.Value;
+            SizeLbl.Content = "Size: " + _width;
+        }
+
+        private void SpeedSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _speed = ((int) SizeSlider.Value) * 100;
+            SpeedLbl.Content = "Speed: " + SpeedSlider.Value;
+        }
+    }
+}
